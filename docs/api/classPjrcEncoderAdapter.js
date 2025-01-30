8d55c0e51ea990d620cb582cb16c320c87aec4ed
Philip Fletcher <philip.fletcher@stutchbury.com>
2025-01-30 13:45:27 +0000

    Update API docs

--- docs/api/classPjrcEncoderAdapter.js
+++ docs/api/classPjrcEncoderAdapter.js
@@ -1,6 +1,7 @@
 var classPjrcEncoderAdapter =
 [
-    [ "PjrcEncoderAdapter", "classPjrcEncoderAdapter.html#a000473943cc1d6b754b2dfa39055ddb9", null ],
+    [ "PjrcEncoderAdapter", "classPjrcEncoderAdapter.html#a6f64db98d202f842150024f1947170e1", null ],
+    [ "~PjrcEncoderAdapter", "classPjrcEncoderAdapter.html#a0edf458eb6edfee39dfc61de38ac602e", null ],
     [ "begin", "classPjrcEncoderAdapter.html#aae0005b4f810d638c6c4c55f820363ea", null ],
     [ "getPosition", "classPjrcEncoderAdapter.html#a99397e1cef205af9c87d6e079e6b6bba", null ],
     [ "setPosition", "classPjrcEncoderAdapter.html#a2f90addcb4fc7bd09e7648e493f54ee2", null ]
